0d1ab5ead6cec217c2cc07bfe2e6bb51bcda87a3
= <huseyintkt41@gmail.com>
2020-12-16 16:35:08 +0100

    bubble-sort

---
+++ bubble-sort/app.js
@@ -0,0 +1,23 @@
+
+function bubbleSort(myArray){
+
+    let myArrayLength = myArray.length;
+
+    for(let i = 0; i < myArrayLength-1; i++){
+        for(let j = 0; j < myArrayLength-i-1; j++){
+
+            if(myArray[j] > myArray[j+1]){
+                let temp = myArray[j];
+                myArray[j] = myArray[j+1];
+                myArray[j+1] = temp;
+            }
+
+        }
+    }
+
+    return myArray;
+}
+
+let myArray = [290, 22, 6, 976, 72, 99];
+
+console.log(bubbleSort(myArray));
